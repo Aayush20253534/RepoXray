580d65aa1335ea9a98d0418c2ca568e711003f74
Aayansh Niranjan <aayansh.singh007i@gmail.com>
2026-04-05 01:24:15 +0530

    Process for dependency graph made

--- server/database.py
+++ server/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-db_url = "postgresql://postgres:postgresaks@localhost:5432/repoxray"
+db_url = "postgresql://postgres:12345@localhost:5432/RepoXray"
 
 engine = create_engine(db_url)
 
