e036fd2e7a75528b2cf30dbd2a011f6880e0f0ea
suryanshcode2601 <suryanshwork2601@gmail.com>
2026-04-04 17:52:21 +0530

    Added user auth with hashed passwords and repo model

---
+++ server/database.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+db_url = "postgresql://postgres:postgresaks@localhost:5432/repoxray"
+
+engine = create_engine(db_url)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
