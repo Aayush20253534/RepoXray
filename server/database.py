8a6362ac578ffdc4d35f97314f8f26554a41833d
suryansh chandel <suryanshwork2601@gmail.com>
2026-04-06 12:30:42 +0530

    Update database.py

--- server/database.py
+++ server/database.py
@@ -1,9 +1,10 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-db_url = "postgresql://postgres:santosh12345@localhost:5432/RepoXray"
+DATABASE_URL = os.environ.get("DATABASE_URL", "")
 
-engine = create_engine(db_url)
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
